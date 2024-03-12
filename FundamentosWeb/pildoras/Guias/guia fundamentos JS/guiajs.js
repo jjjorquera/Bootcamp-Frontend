288385d74b36140c72aee22422437c2a57078a81
Juan Jorquera <jjjuanhugo@gmail.com>
2024-03-12 00:10:49 -0300

    subiendo todo

---
+++ FundamentosWeb/pildoras/Guias/guia fundamentos JS/guiajs.js
@@ -0,0 +1,61 @@
+// var miTitulo = document.querySelector('h1');
+//     miTitulo.innerHTML = '¡Hola Mundo!';
+
+// console.log(miTitulo);
+
+// -------------------------------------------------------------------
+
+// var nuevoH1 = document.createElement('h1');
+// var nuevoParrafo = document.createElement('p');
+
+// var textoH1 = document.createTextNode("Hola mundoooo!!!");
+// var textoParrafo = document.createTextNode ('lorem');
+
+// nuevoH1.appendChild(textoH1);
+// nuevoParrafo.appendChild(textoParrafo);
+
+// // nuevoH1.innerHTML = textoH1;
+// // nuevoParrafo.innerHTML = textoParrafo; con innerHTML no me funciono
+
+// var cabecera = document.getElementById('cabecera');
+
+
+// cabecera.appendChild(nuevoH1);
+// cabecera.appendChild(nuevoParrafo);
+
+
+// console.log(nuevoH1, nuevoParrafo);
+
+//  -------------------------------------------------------------
+
+// Evento Onclick
+
+// function miFunc() {
+    // alert ('Se ha dado click al boton');
+// };
+
+// let p = document.getElementById('foo'); // Encuentra el elemento "p" en el sitio
+// p.onclick = muestraAlerta; //agregamos funcion onclick al elemento.
+
+// function muestraAlerta (evento) {
+//     alert('Evento onclick ejecutandose')
+// };
+
+// ---------------------------------------------------------------
+
+// Evento Onchange
+
+// const selectElement = document.querySelector('.nieve')
+
+// selectElement.addEventListener('change', (event) => {
+//     const resultado = document.querySelector('.resultado');
+//     resultado.textContent = 'Te gusta el sabor ${event.target.value}';
+// });
+
+// let capacitado = true;
+// let edad = 19;
+
+// if (edad < 19 && capacitado) {
+//     alert("Juan Esta autorizando")
+// }
+
