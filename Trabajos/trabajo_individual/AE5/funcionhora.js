cf641defe6752915ead402da3ef012f6b6ec42a0
Juan Jorquera <jjjuanhugo@gmail.com>
2024-03-21 10:58:28 -0300

    modificamos el archivo js de AE5 trabajo individual

--- Trabajos/trabajo_individual/AE5/funcionhora.js
+++ Trabajos/trabajo_individual/AE5/funcionhora.js
@@ -1,13 +1,13 @@
 function mostrarFecha() {
-    var ahora = new Date();
-    var diasRestantes = new Date(ahora.getFullYear() + 1, 0, 1) - ahora;
+    let ahora = new Date();
+    let diasRestantes = new Date(ahora.getFullYear() + 1, 0, 1) - ahora;
 
-    var dias = Math.floor(diasRestantes / (1000 * 60 * 60 * 24));
-    var horas = Math.floor((diasRestantes % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-    var minutos = Math.floor((diasRestantes % (1000 * 60 * 60)) / (1000 * 60));
-    var segundos = Math.floor((diasRestantes % (1000 * 60)) / 1000);
+    let dias = Math.floor(diasRestantes / (1000 * 60 * 60 * 24));
+    let horas = Math.floor((diasRestantes % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    let minutos = Math.floor((diasRestantes % (1000 * 60 * 60)) / (1000 * 60));
+    let segundos = Math.floor((diasRestantes % (1000 * 60)) / 1000);
 
-    var fechaActual = ahora.toLocaleDateString('es-ES', {
+    let fechaActual = ahora.toLocaleDateString('es-ES', {
         weekday: 'long',
         year: 'numeric',
         month: 'long',
@@ -17,11 +17,11 @@ function mostrarFecha() {
         second: 'numeric'
     });
 
-    var mensaje = `Hoy es ${fechaActual}, y son las ${ahora.getHours()} horas, ${ahora.getMinutes()} minutos con ${ahora.getSeconds()} segundos.`;
+    let mensaje = `Hoy es ${fechaActual}, y son las ${ahora.getHours()} horas, ${ahora.getMinutes()} minutos con ${ahora.getSeconds()} segundos.`;
 
     alert(mensaje);
 
-    var contenedorTiempoRestante = document.getElementById('tiemporestante');
+    let contenedorTiempoRestante = document.getElementById('tiemporestante');
     contenedorTiempoRestante.innerHTML = `<h1>Tiempo Restante</h1>
                                           <p>${dias} días, ${horas} horas, ${minutos} minutos y ${segundos} segundos para que termine el año.</p>`;
 }
